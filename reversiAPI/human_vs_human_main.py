c1364d5b46e77bfafc04b2c97e6a68aa1d674f76
KoseiTeramoto <kogupi93@gmail.com>
2019-03-10 04:04:10 +0900

    ADD: human_vs_human_main.py

---
+++ reversiAPI/human_vs_human_main.py
@@ -0,0 +1,23 @@
+from reversiAPI.players import PlayerHuman
+from reversiAPI.utils import ReversiProcessor
+
+def _human_vs_human(white, black):
+    player_white_name = input("player1の名前は?:")
+    player_black_name = input("player2の名前は?:")
+    player_white_class = PlayerHuman(white, player_white_name)
+    player_black_class = PlayerHuman(black, player_black_name)
+    game = ReversiProcessor(
+            player_white_class = player_white_class,
+            player_black_class = player_black_class,
+            options = None,
+            play_game_num = 1,
+            display_board = True,
+            display_result = True
+            )
+    game.progress()
+
+def _main():
+    _human_vs_human(1, -1)
+
+if __name__ == '__main__':
+    main()
