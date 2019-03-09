6a1a458aa9d95f4c0f4ae99340653c568a2a51c0
KoseiTeramoto <kogupi93@gmail.com>
2019-03-10 01:24:39 +0900

    MOD: modify reversi_processor.py

--- reversi_processor.py
+++ reversi_processor.py
@@ -12,5 +12,16 @@ class ReversiProcessor(object):
     This class processes reversi game.
     '''
 
-    def __init__(self):
+    def __init__(
+            self, 
+            player_white_class, 
+            player_black_class, 
+            play_num = 1, 
+            display_board = True, 
+            display_result = True, 
+            stat=1000000
+            ):
+        self.__player_white = player_white_class
+        self.__player_black = player_black_class
+
         
