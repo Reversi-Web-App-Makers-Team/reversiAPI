0c5420caa256144706bf3efdeb92bcfd3f5b6ffd
KoseiTeramoto <kogupi93@gmail.com>
2019-03-09 18:45:07 +0900

    MOD: modify variable name.

--- reversi_packages.py
+++ reversi_packages.py
@@ -5,22 +5,22 @@ import numpy as np
 class ReversiPackages(object):
 
     '''
-    This class is packages used in organizing reversi game
+    This class is packages used in reversi_processor.py
     '''
     
-    def __init__(self, board=None, options=None, printing=True):
+    def __init__(self, board=None, options=None, display_board=True):
         
         '''
         Initialize class parameters.
         
         Args:
-            printing(boolean):
-                False -> not printing board.
-                True -> printing board.
+            display_board(boolean):
+                False -> not display board
+                True -> display board
 
             board(list):
                 shape = (total_square_num=64, 1)
-                instructions: board state.
+                instructions: board state
                               empty(no stone) -> 0
                               white stone -> 1
                               brack stone -> -1
@@ -29,7 +29,7 @@ class ReversiPackages(object):
                 global parameters.
         '''
         
-        # self.__options is global parameters.
+        # self.__options is global parameters
         if options == None:
             self.__options = toml.load('./settings.toml')['REVERSI_PACKAGES']
         else:
@@ -43,18 +43,18 @@ class ReversiPackages(object):
             for index in range(self.__options['SQUARE_NUM']):
                 self.__board.append(self.__options['EMPTY'])
                 
-            # set initial 2 white stones.
+            # set initial 2 white stones
             for initial_white_place in self.__options['INITIAL_WHITE_PLACES']:
                 self.__board[initial_white_place] = self.__options['WHITE']
 
-            # set initial 2 black stones.
+            # set initial 2 black stones
             for initial_black_place in self.__options['INITIAL_BLACK_PLACES']:
                 self.__board[initial_black_place] = self.__options['BLACK']
             
         else:
             self.__board = board
             
-        # self.__winner flag is used when checking winner.
+        # self.__winner flag is used when checking winner
         self.__winner == None
 
         # self.__reversable_stone_number_dict: dictionary
@@ -62,16 +62,17 @@ class ReversiPackages(object):
         #   values: list of how many stones reversed when putting stone there for all 8 directions
         self.__reversable_stone_number_dict = {}
 
-        # make class variables for printing board.
-        if printing:
+        self.__display_board = display_board
+        # make class variables for displaying board
+        if self.__display_board:
 
             # converter dictinary (1, -1, 0 -> "⚪️", " ⚫️", "None")
             self.__marks = toml.load('./settings.toml')['MARKS']
 
             # number board (1 ~ 64) for displaying
-            self.__index_board_for_print = []
+            self.__index_board_for_displaying = []
             for index in range(self.__options['SQUARE_NUM']):
-                self.__index_board_for_print.append(index + 1)
+                self.__index_board_for_displaying.append(index + 1)
 
 
     def check_winner(self):
@@ -80,7 +81,7 @@ class ReversiPackages(object):
         This function check which player wins (black stone player of white stone player) or draw.
             if winner is white, self.__winner <- 1
             if winner is brack, self.__winner <- -1
-            if draw, self.__winner <-  2
+            if draw, self.__winner <- 2
         '''
 
         sum_score = sum(self.__board)
@@ -114,7 +115,7 @@ class ReversiPackages(object):
                 black -> -1
         '''
 
-        # putting new stone.
+        # putting new stone
         self.__board[putting_index] = player
 
         # reversing stone for all 8 directions adapting to self.__reversable_stone_number_dict
@@ -151,22 +152,26 @@ class ReversiPackages(object):
             self.__board[putting_index - 9 * i] *= self.__options['CHANGE_COLOR']
 
     
-    def print_board(self):
-        temp_board = []
+    def display_board(self):
+        if self.__display_board:
+            temp_board = []
 
-        # convert (1, -1, 0) -> ("⚪️", " ⚫️", "None")
-        for i in self.board:
-            temp_board.append(marks[i])
+            # convert (1, -1, 0) -> ("⚪️", " ⚫️", "None")
+            for i in self.__board:
+                temp_board.append(marks[i])
 
-        # convert "None" -> index (1 ~ 64)
-        for i in range(self.__options['SQUARE_NUM']):
-            if temp_board[i] == " ":
-                temp_board[i] = "%02d" % self.index_board[i]
+            # convert "None" -> index (1 ~ 64)
+            for i in range(self.__options['SQUARE_NUM']):
+                if temp_board[i] == " ":
+                    temp_board[i] = "%02d" % self.__index_board_for_displaying[i]
 
-        # lines
-        row = " {} | {} | {} | {} | {} | {} | {} | {} "
-        hr = "\n---------------------------------------\n"
+            # lines
+            row = " {} | {} | {} | {} | {} | {} | {} | {} "
+            hr = "\n---------------------------------------\n"
 
-        # printing board
-        print((row + hr + row + hr + row + hr + row + hr + row + hr + row + hr + row + hr + row).format(*temp_board))
+            # printing board
+            print((row + hr + row + hr + row + hr + row + hr + row + hr + row + hr + row + hr + row).format(*temp_board))
+
+        else:
+            return
 
