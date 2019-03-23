8392cf4beb808d90f711d61827bfeff91e71fe94
terapoon <teramoto@pgv.co.jp>
2019-03-24 00:27:04 +0900

    MOD: upgrade versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
     from pip.req import parse_requirements
 setup(
     name = 'reversiAPI',
-    version='1.0.2',
+    version='1.0.3',
     url = 'https://github.com/reversiWebApp/reversiAPI.git',
     license = 'Free',
     author = 'Kosei Teramoto',
