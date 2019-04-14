b86d7df7d2fc2a5f6fb8c7fc2c8172c8d0eedc2c
Shion-0014 <seanmqe@gmail.com>
2019-04-14 16:06:39 +0900

    "Add: new_model_class"

--- reversiAPI/players/models/models.py
+++ reversiAPI/players/models/models.py
@@ -2,10 +2,10 @@ import torch.nn.functional as F
 from torch import nn
 
 
-class TheModelClass(nn.Module):
+class TheModelClass1(nn.Module):
 
     def __init__(self, n_in, n_mid, n_out):
-        super(TheModelClass, self).__init__()
+        super(TheModelClass1, self).__init__()
         self.fc1 = nn.Linear(n_in, n_mid)
         self.fc2 = nn.Linear(n_mid, n_mid)
         self.fc3 = nn.Linear(n_mid, n_out)
@@ -14,4 +14,79 @@ class TheModelClass(nn.Module):
         h1 = F.relu(self.fc1(x))
         h2 = F.relu(self.fc2(h1))
         output = self.fc3(h2)
+        return output
+
+
+class TheModelClass2(nn.Module):
+
+    def __init__(self, n_in, n_mid, n_out):
+        super(TheModelClass2, self).__init__()
+        self.fc1 = nn.Linear(n_in, n_mid)
+        self.fc2 = nn.Linear(n_mid, n_mid)
+        self.fc3 = nn.Linear(n_mid, n_mid)
+        # Dueing Network
+        self.fc4_adv = nn.Linear(n_mid, n_out)
+        self.fc4_v = nn.Linear(n_mid, 1)
+
+    def forward(self, x):
+        h1 = F.leaky_relu(self.fc1(x))
+        h2 = F.leaky_relu(self.fc2(h1))
+        h3 = F.leaky_relu(self.fc3(h2))
+        adv = self.fc4_adv(h3)
+        val = self.fc4_v(h3).expand(-1, adv.size(1))
+
+        output = val + adv - adv.mean(1, keepdim=True).expand(-1, adv.size(1))
+
+        return output
+
+
+class TheModelClassSl1(nn.Module):
+
+    def __init__(self):
+        super(TheModelClassSl1, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+
+        self.fc1 = nn.Linear(64*4*4, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 64)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, (2, 2))
+
+        x = x.view(-1, 64*4*4)
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        output = self.fc3(x)
+
+        return output
+
+class TheModelClassSl2(nn.Module):
+
+    def __init__(self):
+        super(TheModelClassSl2, self).__init__()
+
+        self.conv1 = nn.Conv2d(2, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+
+        self.fc1 = nn.Linear(64*4*4, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 64)
+
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, (2, 2))
+
+        x = x.view(-1, 64*4*4)
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        output = self.fc3(x)
+
         return output
