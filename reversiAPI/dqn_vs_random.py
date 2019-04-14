78842cacc616f627a38bc61c79f924bf1f9dd4cc
Shion-0014 <seanmqe@gmail.com>
2019-04-14 14:21:45 +0900

    a

--- reversiAPI/dqn_vs_random.py
+++ reversiAPI/dqn_vs_random.py
@@ -1,19 +1,23 @@
 import os
+import sys
+sys.path.append('/Users/shion/AnacondaProjects/reversi_project/reversiAPI')
+
 
 from reversiAPI.players.dqnkun import PlayerDqn
 from reversiAPI.players.randomkun import PlayerRandom
 from reversiAPI.utils.reversi_processor import ReversiProcessor
 from reversiAPI.utils.settings import DQN
+from reversiAPI.players.slkun import PlayerSl
 
 
 def _dqn_vs_random(white, black, file_path):
-    player_white_instance = PlayerRandom(white, False)
-    player_black_instance = PlayerDqn(black, file_path, False)
+    player_white_instance = PlayerSl(white, file_path, False)
+    player_black_instance = PlayerRandom(black, False)
     game = ReversiProcessor(
         player_white_instance=player_white_instance,
         player_black_instance=player_black_instance,
         options=None,
-        play_game_num=100,
+        play_game_num=10000,
         display_board=False,
         display_result=True
     )
